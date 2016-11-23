ec31c7afb69f4dbe4265f093fec4b6ee6c46cade
Ragoo <dasmotte@gmx.net>
2016-11-23 20:37:13 +0100

    test new navbar

---
+++ ferienhausWeb/static/js/navbar.js
@@ -0,0 +1,13 @@
+/**
+ * Created by Philipp on 23.11.2016.
+ */
+
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
