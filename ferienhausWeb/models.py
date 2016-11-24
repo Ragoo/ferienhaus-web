98bfb9bf1994e50502f0b8c150aafe60c970971f
Ragoo <dasmotte@gmx.net>
2016-11-24 22:08:25 +0100

    scrollbars

--- ferienhausWeb/models.py
+++ ferienhausWeb/models.py
@@ -1,5 +1,10 @@
 from django.db import models
 from django.utils import timezone
+import os
+
+
+def get_image_path(instance, filename):
+    return os.path.join('pictures', filename)
 
 class Post(models.Model):
     author = models.ForeignKey('auth.User')
@@ -17,3 +22,10 @@ class Post(models.Model):
     def __str__(self):
         return self.title
 
+class Trip(models.Model):
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    picture = models.FileField(upload_to=get_image_path, blank=True, null=True)
+
+    def __str__(self):
+        return self.title
