32c50b9317cec9409f0e6c9dd61507d18b5518ed
Ragoo <dasmotte@gmx.net>
2019-08-31 13:09:40 +0200

    fix media bug

--- ferienhaus/urls.py
+++ ferienhaus/urls.py
@@ -25,6 +25,7 @@ urlpatterns = [
     url(r'^login/$', django.contrib.auth.views.login, name='login'),
     url(r'^logout/$', django.contrib.auth.views.logout, {'next_page': '/'}, name='logout'),
     url(r'^ckeditor/', include('ckeditor_uploader.urls')),
+    url(r'^media/(?P<path>.*)$', django.views.static.serve,{'document_root': settings.MEDIA_ROOT, 'show_indexes': True})
 ]
 
 if settings.DEBUG:
